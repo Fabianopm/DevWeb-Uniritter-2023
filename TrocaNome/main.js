4d13c3621965206c953926c150bd18fb0431759f
Fabiano Pacheco <fabianopmonteiro@gmail.com>
2023-04-16 22:43:59 -0300

    alteração realizada

--- TrocaNome/main.js
+++ TrocaNome/main.js
@@ -1,7 +1,8 @@
 function inverterFrase(inputFrase, resultado) {
   const input = document.getElementById("inputFrase");
-  const fraseOriginal = input.value;
-  const fraseInvertida = fraseOriginal.split(" ").reverse().join(" ");
+  const fraseOriginal = input.value; //Pegar o valor da frase digitada
+  const fraseInvertida = fraseOriginal.split(" ").reverse().join(" "); //split para dividir a string e coloca-la em um array,
+  //reverse para iverter os itens do array. E o Joim para juntar todos os elementos do array em uma string e retorna a mesma.
   const resultadoElement = document.getElementById("resultado");
-  resultadoElement.innerHTML = fraseInvertida;
+  resultadoElement.innerHTML = fraseInvertida; //Mostrar o resultado na tela.
 }
